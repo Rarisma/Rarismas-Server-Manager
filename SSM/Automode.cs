e2e074173dd9a3f076f423093f33c13e24611828
Rarisma <49795711+Rarisma@users.noreply.github.com>
2021-05-07 11:29:19 +0100

    Update Automode.cs

--- SSM/Automode.cs
+++ SSM/Automode.cs
@@ -1,31 +1,31 @@
-using system;
+using System;
+using System.Collections.Generic;
 //One last ride to dreamland.
 namespace SSM
 {
-  class Automode()
-  {
-
-    public static void CreatePaperServer()
-    {
-       ServerInfo.ServerVariant = "Paper";
-       LibRarisma.IO.DownloadFile("https://raw.githubusercontent.com/Rarisma/Simple-Server-Manager/main/ServerFiles/Minecraft/Paper", AppDomain.CurrentDomain.BaseDirectory + "//Cache//","Paper");
-       List<String> ServerReader = new();
-       ServerReader.AddRange(System.IO.File.ReadAllLines(AppDomain.CurrentDomain.BaseDirectory + "//Cache//Paper"));
-       ServerInfo.ServerVersion = ServerReader[0];
-       ServerInfo.Ram = Convert.ToInt64(LibRarisma.IO.GetRAM()) - 4096; //Should leave enough for the user to play aswell
-       SSMGeneric.BuildServer(ServerReader[1]);
-    }
+	class Automode 
+	{
+    	public static void CreatePaperServer()
+    	{
+       		ServerInfo.ServerVariant = "Paper";
+       		LibRarisma.IO.DownloadFile("https://raw.githubusercontent.com/Rarisma/Simple-Server-Manager/main/ServerFiles/Minecraft/Paper", AppDomain.CurrentDomain.BaseDirectory + "//Cache//","Paper");
+       		List<String> ServerReader = new();
+       		ServerReader.AddRange(System.IO.File.ReadAllLines(AppDomain.CurrentDomain.BaseDirectory + "//Cache//Paper"));
+       		ServerInfo.ServerVersion = ServerReader[0];
+       		ServerInfo.Ram = Convert.ToInt64(LibRarisma.IO.GetRAM()) - 4096; //Should leave enough for the user to play aswell
+       		SSMGeneric.BuildServer(ServerReader[1]);
+      }
+    	
+		public static void CreateForgeServer()
+    	{
+			    ServerInfo.ServerVariant = "Forge";
+       		LibRarisma.IO.DownloadFile("https://raw.githubusercontent.com/Rarisma/Simple-Server-Manager/main/ServerFiles/Minecraft/Forge", AppDomain.CurrentDomain.BaseDirectory + "//Cache//","Forge");
+       		List<String> ServerReader = new();
+       		ServerReader.AddRange(System.IO.File.ReadAllLines(AppDomain.CurrentDomain.BaseDirectory + "//Cache//Forge"));
+       		ServerInfo.ServerVersion = ServerReader[0];
+       		ServerInfo.Ram = Convert.ToInt64(LibRarisma.IO.GetRAM()) - 4096; //Should leave enough for the user to play aswell
+       		SSMGeneric.BuildServer(ServerReader[1]);
+    	}
     
-    public static void CreateForgeServer()
-    {
-       ServerInfo.ServerVariant = "Forge";
-       LibRarisma.IO.DownloadFile("https://raw.githubusercontent.com/Rarisma/Simple-Server-Manager/main/ServerFiles/Minecraft/Forge", AppDomain.CurrentDomain.BaseDirectory + "//Cache//","Forge");
-       List<String> ServerReader = new();
-       ServerReader.AddRange(System.IO.File.ReadAllLines(AppDomain.CurrentDomain.BaseDirectory + "//Cache//Forge"));
-       ServerInfo.ServerVersion = ServerReader[0];
-       ServerInfo.Ram = Convert.ToInt64(LibRarisma.IO.GetRAM()) - 4096; //Should leave enough for the user to play aswell
-       SSMGeneric.BuildServer(ServerReader[1]);
-    }
-    
-  }
+	}
 }
