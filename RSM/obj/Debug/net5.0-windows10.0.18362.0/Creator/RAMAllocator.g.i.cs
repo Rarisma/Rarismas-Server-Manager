369cc390da96082efa8fef517939f34d2bf66127
Rarisma <49795711+Rarisma@users.noreply.github.com>
2021-05-13 18:04:25 +0100

    Moved solution
    Autobuild stop emailing me

--- RSM/obj/Debug/net5.0-windows10.0.18362.0/Creator/RAMAllocator.g.i.cs
+++ RSM/obj/Debug/net5.0-windows10.0.18362.0/Creator/RAMAllocator.g.i.cs
@@ -83,7 +83,7 @@ namespace RSM.Creator {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/RSM;component/creator/ramallocator.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/RSM;V1.0.0.0;component/creator/ramallocator.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Creator\RAMAllocator.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
--- RSM/obj/Debug/net5.0-windows10.0.18362.0/Creator/VersionSelecter.g.i.cs
+++ RSM/obj/Debug/net5.0-windows10.0.18362.0/Creator/VersionSelecter.g.i.cs
@@ -91,7 +91,7 @@ namespace RSM.Creator {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/RSM;component/creator/versionselecter.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/RSM;V1.0.0.0;component/creator/versionselecter.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Creator\VersionSelecter.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
